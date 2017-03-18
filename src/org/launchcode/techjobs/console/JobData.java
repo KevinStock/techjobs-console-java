ca5b42892f7d8a99a267acd2fd8d672b44b13bb2
kevinstock <kevin@kevinstock.net>
2017-03-18 11:06:32 -0500

    missing toLowerCase added to comparison

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -86,7 +86,7 @@ public class JobData {
 
             String aValue = row.get(column).toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
